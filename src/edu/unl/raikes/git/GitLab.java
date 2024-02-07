b262199332c5e2fd2419bb0963346af4269f1739
Banderson03 <112013412+Banderson03@users.noreply.github.com>
2024-02-07 11:30:24 -0600

    More random things and such

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,7 +2,8 @@ package edu.unl.raikes.git;
 
 public class GitLab {
 	public static void main(String[] args) {
-		GitLabHelper helper = new GitLabHelper("Tage", "Bennett");
+		GitLabHelper helper = new GitLabHelper(new String[] {"Bennett",
+		"Tage"});
 		helper.PrintWelcomeMessage();
 	}
 }
