a39278cc3ca36865b741821ce652fbbf9c602d68
TageZ <tagezerby@gmail.com>
2024-02-07 11:25:22 -0600

    stuff and things

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -1,5 +1,8 @@
 package edu.unl.raikes.git;
 
 public class GitLab {
-
+	public static void main(String[] args) {
+		GitLabHelper helper = new GitLabHelper("Tage", "Bennett");
+		helper.PrintWelcomeMessage();
+	}
 }
